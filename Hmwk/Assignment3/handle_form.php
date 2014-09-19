b071a49a96c875e08ef3a6c8ac00568b2746ca91
poman1 <poman@student.rcc.edu>
2014-09-19 10:57:21 -0700

    Script 2.4 - handle_form.php #3

--- Hmwk/Assignment3/handle_form.php
+++ Hmwk/Assignment3/handle_form.php
@@ -3,37 +3,72 @@
 <head>
 <meta http-equiv="Content-Type" content="text/html; charset=utf-8" />
 <title>Form Feedback</title>
+<style type="text/css" title="text/css" media="all">
+ 	.error {
+ 	font-weight: bold;
+ 	color: #C00;
+}
+</style>
+
 </head>
 
 <?php 
-# Script 2.3 - handle_form.php
+# Script 2.4 - handle_form.php #3
 
-//Create a shorthand for the form data:
-$name=$_REQUEST['name'];
-$email=$_REQUEST['email'];
-$comments=$_REQUEST['comments'];
+// Validate the name:
+ if (!empty($_REQUEST['name'])) {
+ $name = $_REQUEST['name'];
+ } else {
+ $name = NULL;
+ echo '<p class="error">You forgot to enter your name!</p>';
+ }
 
-//Create the $gender variable:
-if (isset($_REQUEST['gender'])) {
-	$gender=$_REQUEST['gender'];
-}else{
-	$gender=NULL;
-}
+ // Validate the email:
+ if (!empty($_REQUEST['email'])) {
+ $email = $_REQUEST['email'];
+ } else {
+ $email = NULL;
+ echo '<p class="error">You forgot to enter your email address!</p>';
+ }
 
-//Print the submitted information
-echo "<p>Thank you, <b>$name</b>,for the following comments:<br /><tt>$comments</tt></p>
-<p>We will reply to you at<i> $email</i>.</p>\n";
+ // Validate the comments:
+ if (!empty($_REQUEST['comments'])) {
+ $comments = $_REQUEST['comments'];
+ } else {
+ $comments = NULL;
+ echo '<p class="error">You forgot to enter your comments!</p>';
+ }
 
-//Print a message based upon the gender value:
-if ($gender == 'M'){
-	echo '<p><b>Good day, Sir!</b></p>';
-}elseif($gender == 'F') {
-	echo '<p><b>Good day, Madam!</b></p>';
-}else{//No gender selected.
-	echo '<p><b>You forgot to enter you gender!</b></p>';
-}
+ // Validate the gender:
+ if (isset($_REQUEST['gender'])) {
 
-?>
-<body>
-</body>
-</html>
+ $gender = $_REQUEST['gender'];
+
+ if ($gender == 'M') {
+ echo '<p><b>Good day, Sir!</b></p>';
+ }elseif ($gender == 'F') {
+ echo '<p><b>Good day, Madam!</b></p>';
+ }else { // Unacceptable value.
+ $gender = NULL;
+ echo '<p class="error">Gender should be either "M" or "F"!</p>';
+ }
+
+ } else { // $_REQUEST['gender'] is not set.
+ $gender = NULL;
+ echo '<p class="error">You forgot to select your gender!</p>';
+ }
+
+ // If everything is OK, print the message:
+ if ($name && $email && $gender && $comments) {
+
+ echo "<p>Thank you, <b>$name</b>, for the following comments:<br />
+ <tt>$comments</tt></p>
+ <p>We will reply to you at <i>$email</i>.</p>\n";
+
+ } else { // Missing form value.
+ echo '<p class="error">Please go back and fill out the form again.</p>';
+ }
+
+ ?>
+ </body>
+ </html>
