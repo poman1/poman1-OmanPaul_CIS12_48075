01c3507c5de2d13dc8a22ee8db5054080807c139
poman1 <poman@student.rcc.edu>
2014-09-19 10:45:15 -0700

    Script 2.3

--- Hmwk/Assignment3/handle_form.php
+++ Hmwk/Assignment3/handle_form.php
@@ -6,25 +6,34 @@
 </head>
 
 <?php 
-# Script 2.2 - handle_form.php
+# Script 2.3 - handle_form.php
 
 //Create a shorthand for the form data:
 $name=$_REQUEST['name'];
 $email=$_REQUEST['email'];
 $comments=$_REQUEST['comments'];
 
-/*Not used:
-$_REQUEST['age'];
-$_REQUEST['gender'];
-$_REQUEST['submit'];
-*/
+//Create the $gender variable:
+if (isset($_REQUEST['gender'])) {
+	$gender=$_REQUEST['gender'];
+}else{
+	$gender=NULL;
+}
 
 //Print the submitted information
-echo "<p>Thank you, <b>$name</b> for the following comments:<br />
-<tt>$comment</tt></p>
-<p>We will reply to you at <i>$email</i>.</p>\n";
-?>
+echo "<p>Thank you, <b>$name</b>,for the following comments:<br /><tt>$comments</tt></p>
+<p>We will reply to you at<i> $email</i>.</p>\n";
 
+//Print a message based upon the gender value:
+if ($gender == 'M'){
+	echo '<p><b>Good day, Sir!</b></p>';
+}elseif($gender == 'F') {
+	echo '<p><b>Good day, Madam!</b></p>';
+}else{//No gender selected.
+	echo '<p><b>You forgot to enter you gender!</b></p>';
+}
+
+?>
 <body>
 </body>
 </html>
